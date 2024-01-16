ef2bb1201174e6168f0184ee9917aedf50b034b9
djoldes <davejoldes@gmail.com>
2024-01-16 14:37:18 +0200

    added searchbar and its features + few more items to the list

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/partials/header/header.component";
 import { HomeComponent } from './components/pages/home/home.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, HeaderComponent, HomeComponent,]
+    imports: [CommonModule, RouterOutlet, HeaderComponent, HomeComponent, FormsModule]
 })
 export class AppComponent {
   title = 'frontend';
--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -3,17 +3,19 @@ import { Food } from '../../../shared/models/Food';
 import { FoodService } from '../../../services/food.service';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements OnInit {
   
   foods:Food[] = [];
+  searchText: string = ''; //si asta e pt searchbar
   constructor(private foodService:FoodService, private router:Router){
     this.foods = foodService.getAll();
   }
@@ -26,4 +28,14 @@ export class HomeComponent implements OnInit {
     this.router.navigate(['/food', numericFoodId]);
   }
 
+  //pt searchbar de aici in jos
+  filterFoods(): Food[]{
+    if(!this.searchText){
+      return this.foods;
+    }
+    return this.foods.filter((food) => 
+      food.tags?.some((tag) => tag.toLowerCase().includes(this.searchText.toLowerCase()))
+      );
+  }
+  //pana aici
 }
