f875f124d39baed8cef78baa324b0c7af2f7a11e
djoldes <davejoldes@gmail.com>
2024-01-17 12:17:08 +0200

    finished the cart + fixed the header + added not found component

--- frontend/src/app/components/pages/cart-page/cart-page.component.ts
+++ frontend/src/app/components/pages/cart-page/cart-page.component.ts
@@ -3,19 +3,22 @@ import { Cart } from '../../../shared/models/Cart';
 import { CartService } from '../../../services/cart.service';
 import { CartItem } from '../../../shared/models/CartItem';
 import { TitleComponent } from '../../partials/title/title.component';
-import { Food } from '../../../shared/models/Food';
+import { NgFor } from '@angular/common';
+import { FoodPageComponent } from '../food-page/food-page.component';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cart-page',
   standalone: true,
-  imports: [TitleComponent, Food],
+  imports: [TitleComponent, NgFor, FoodPageComponent, RouterModule, CommonModule],
   templateUrl: './cart-page.component.html',
   styleUrl: './cart-page.component.css'
 })
 export class CartPageComponent implements OnInit{
   
   cart!:Cart;
-  constructor(private cartService: CartService){
+  constructor(private cartService: CartService,){
     this.cartService.getCartObservable().subscribe((cart) => {
       this.cart = cart;
     })
--- frontend/src/app/components/partials/header/header.component.ts
+++ frontend/src/app/components/partials/header/header.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit} from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { CartService } from '../../../services/cart.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent implements OnInit{
   
-  constructor(){}
+  cartQuantity = 0;
+
+  constructor(cartService: CartService){
+    cartService.getCartObservable().subscribe((newCart) => {
+      this.cartQuantity = newCart.totalCount;
+    })
+  }
   
   ngOnInit(): void {
     throw new Error('Method not implemented.');
